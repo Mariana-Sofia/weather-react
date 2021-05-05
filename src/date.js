fc161e6bcc3a0c1f02b76358cf8a43f611e7666a
Mariana-Sofia <79910102+Mariana-Sofia@users.noreply.github.com>
2021-05-05 16:14:35 +0100

    Setting Hour and Date

--- src/date.js
+++ src/date.js
@@ -3,11 +3,60 @@ import './date.css';
 
 
 export default function todayDate(){
+    let today=new Date();
+
+    let hour=today.getHours();
+    if(hour <10){
+        hour=`0${hour}`;
+    }
+    let minutes=today.getMinutes();
+    if (minutes<10){
+        minutes=`0${minutes}`;
+    }
+
+    let seconds=today.getSeconds();
+    if (seconds<10){
+        seconds=`0${seconds}`;
+    }
+
+    let months=[
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ]
+
+      let month=months[today.getMonth()];
+      let weekday=days[today.getDay()];
+      let day = today.getDate();
+      let year = today.getFullYear();
+
     return(
-        <span>
-            <p className="todayDate">
-            Today is 05 of September,2021. 15h00
-            </p>
-        </span>
+        <div>
+            <span className="todayDate">
+                {weekday}, {day} of {month}, {year}
+            </span>
+            <span className="todayHour">
+                {hour}:{minutes}:{seconds}
+            </span>
+        </div>
         )
 }
